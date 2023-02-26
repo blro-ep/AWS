e89d0ae2cd67d49f3b512f1ca9b63710c7da9d82
RogerBlum <roger.blum@edu.tbz.ch>
2023-02-26 16:13:15 +0100

    opt. authorize-security-group-ingress

--- cli/create_ec2_instance.sh
+++ cli/create_ec2_instance.sh
@@ -1,23 +1,67 @@
 #!/bin/bash
 
-#variables
-KeyPair="id_aws"
-SecGroNam="my-security-group"
-SecGroDes="My security group"
+# variables
+AWS_MACHINE_IMAGE="ami-0d1ddd83282187d18"
+AWS_REGION="eu-central-1"
+INSTANCE_TYPE="t2.micro"
+INSTANCE_NAME="my-ec2-intance"
+KEY_PAIR_NAME="id_aws"
+KEY_PATH_LOCAL="/home/outside/.ssh/"
+SECURITY_GROUP_NAME="my-security-group"
+SECURITY_GROUP_DESCRIPTION="My security group"
 
-# key-pair erstellen und privateKey lokal speichern
-aws ec2 create-key-pair --key-name $KeyPair --query 'KeyMaterial' --output text > ~/.ssh/$KeyPair.pem
+# check if the key already exist local
+if [ -f $KEY_PATH_LOCAL$KEY_PAIR_NAME.pem ];
+    then
+        echo "Yes, existe."
+        rm -f $KEY_PATH_LOCAL$KEY_PAIR_NAME.pem
+    else
+    echo "No, no existe"
+fi
+
+# create key-pair and save the privateKey local
+KEY_ID=$(aws ec2 create-key-pair \
+    --region $AWS_REGION \
+    --key-name $KEY_PAIR_NAME \
+    --query 'KeyMaterial' \
+    --output text > $KEY_PATH_LOCAL$KEY_PAIR_NAME.pem) && \
+echo "added Key $KEY_PATH_LOCAL$KEY_PAIR_NAME.pem"
 
 # change private-key permission
-chmod 400 id_aws.pem
+chmod 400 $KEY_PATH_LOCAL$KEY_PAIR_NAME.pem
 
 # create security-group
-aws ec2 create-security-group --group-name $SecGroNam --description $SecGroDes
+SECURITY_GROUP_ID=$(aws ec2 create-security-group \
+    --group-name "$SECURITY_GROUP_NAME" \
+    --description "$SECURITY_GROUP_DESCRIPTION" \
+    --query 'GroupId' \
+    --output text) && \
+echo "Security group created with id $SECURITY_GROUP_ID"
 
-# authorize-security-group-ingress
-aws ec2 authorize-security-group-ingress --group-id sg-08499662d6985e9bb --protocol tcp --port 22 --cidr 0.0.0.0/24
+# add inbound rules
+SECURITY_GROUP_RULE=$(aws ec2 authorize-security-group-ingress \
+    --group-id $SECURITY_GROUP_ID \
+    --protocol tcp \
+    --port 22 \
+    --cidr 0.0.0.0/0 \
+    --output text) && \
+echo "Security group rule created $SECURITY_GROUP_RULE"
 
+# create ec2 instance
+INSTANCE_ID=$(aws ec2 run-instances \
+    --region $AWS_REGION \
+    --image-id $AWS_MACHINE_IMAGE \
+    --count 1 \
+    --instance-type $INSTANCE_TYPE \
+    --key-name $KEY_PAIR_NAME \
+    --security-group-ids $SECURITY_GROUP_ID \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" \
+    --query 'Instances[0].InstanceId' \
+    --output text) && \
+echo "Instance launched with id $INSTANCE_ID"
 
-
- 
+INSTANCE_IP=$(aws ec2 describe-instances \
+    --instance-ids $INSTANCE_ID \
+    --query "Reservations[0].Instances[0].PublicIpAddress" --output text) && \
+echo "EC2 instance $INSTANCE_NAME IP: $INSTANCE_IP"
 
